6e8dddc32363950ddb9488c75a48cba4aa372f8f
Damian Cichosz <damian.cichosz97@gmail.com>
2018-08-15 17:32:19 +0200

    v0.8

---
+++ Pharmacy-Manager/Pharmacy-Manager/Exceptions/QuantityException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Pharmacy_Manager
+{
+	/// <summary>
+	/// 	Aby wywołać wyjątek, gdy podano ilość do kupienia większą od ilości leku w bazie
+	/// </summary>
+
+	public class QuantityException : Exception
+	{ }
+}
---
+++ Pharmacy-Manager/Pharmacy-Manager/Exceptions/ZeroException.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace Pharmacy_Manager
+{
+	/// <summary>
+	/// 	Aby wywołać wyjątek -> gdy transakcja nie zmieni żadnego wiersza | lub zmieni tylko 1 w bazie logów!!!!
+	/// </summary>
+
+	public class ZeroException : Exception
+	{ }
+}
